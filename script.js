0e16364679faea52a253e1a3f1680f8c79cc9afe
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-04-17 19:23:28 +0530

    final touch up

--- script.js
+++ script.js
@@ -52,64 +52,66 @@ function getComputerChoice() {
   }
   
   // ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
-
   function showResult(score, playerChoice, computerChoice) {
-  // Hint: on a score of -1
-  // You should do result.innerText = 'You Lose!'
-  // Don't forget to grab the div with the 'result' id!
-        let result  = document.getElementById('result')
-        switch (score) {
-            case -1:
-                result.inneText = 'You Lose!'
-                break;
-            case 0:
-                result.innerText = 'You Win!'
-                break;
-            case 1:
-                result.innerText = 'You Win!'
-                break;
-        }
-        let payerScore = document.getElementById('player-score')
-        let hands = document.getElementById('hands')
-        playerScore.innerText = `${Number(playerScore.innerText) + score}`
-            hands.innerText = `👱 ${playerChoice} vs 🤖 ${computerChoice}`
+    // Hint: on a score of -1
+    // You should do result.innerText = 'You Lose!'
+    // Don't forget to grab the div with the 'result' id!
+    
+    let result = document.getElementById('result')
+    switch (score) {
+      case -1:
+        result.innerText = `You Lose!`
+        break;
+      case 0:
+        result.innerText = `It's a Draw!`
+        break;
+      case 1:
+        result.innerText = `You Win!`
+        break;
+    }
+  
+    let playerScore = document.getElementById('player-score')
+    let hands = document.getElementById('hands')
+    playerScore.innerText = `${Number(playerScore.innerText) + score}`
+      hands.innerText = `👱 ${playerChoice} vs 🤖 ${computerChoice}`
   }
-
-// ** Calculate who won and show it on the screen **
-function onClickRPS(playerChoice) {
+  
+  // ** Calculate who won and show it on the screen **
+  function onClickRPS(playerChoice) {
     const computerChoice = getComputerChoice()
     const score = getResult(playerChoice.value, computerChoice)
     showResult(score, playerChoice.value, computerChoice)
-}
-
-// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
-function playGame() {
+  }
+  
+  // ** Make the RPS buttons actively listen for a click and do something once a click is detected **
+  function playGame() {
     // use querySelector to select all RPS Buttons
     let rpsButtons = document.querySelectorAll('.rpsButton')
-    // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
   
-  // 1. loop through the buttons using a forEach loop
-  // 2. Add a 'click' event listener to each button
-  // 3. Call the onClickRPS function every time someone clicks
-  // 4. Make sure to pass the currently selected rps button as an argument
-  rpsButtons.forEach(rpsButton => {
-    rpsButton.onclick = () => onClickRPS(rpsButton)
-  })
-  // Add a click listener to the end game button that runs the endGame() function on click
-  let endgameButton = document.getElementById('endGameButton')
-  endGameButton.onclick = () => endGame()
-}
-
-// ** endGame function clears all the text on the DOM **
-
-function endGame() {
+    // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
+    
+    // 1. loop through the buttons using a forEach loop
+    // 2. Add a 'click' event listener to each button
+    // 3. Call the onClickRPS function every time someone clicks
+    // 4. Make sure to pass the currently selected rps button as an argument
+  
+    rpsButtons.forEach(rpsButton => {
+      rpsButton.onclick = () => onClickRPS(rpsButton)
+    })
+  
+    // Add a click listener to the end game button that runs the endGame() function on click
+    let endGameButton = document.getElementById('endGameButton')
+    endGameButton.onclick = () => endGame()
+  }
+  
+  // ** endGame function clears all the text on the DOM **
+  function endGame() {
     let playerScore = document.getElementById('player-score')
     let hands = document.getElementById('hands')
     let result = document.getElementById('result')
     playerScore.innerText = ''
     hands.innerText = ''
     result.innerText = ''
-
-}
-
-playGame()
+  }
+  
+  playGame()
